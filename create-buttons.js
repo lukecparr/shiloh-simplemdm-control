603e9975b00847d95fecb33e3f144c6693d6b65e
lukecparr <lukec.parr@pm.me>
2021-11-01 10:50:10 -0400

    Added ability to programatically create buttons for multiple stations

--- create-buttons.js
+++ create-buttons.js
@@ -1,3 +1,16 @@
+const stations = [
+	{
+		id: '583116',
+		name: 'Check-in (1)',
+		blockId: 'block-3b6912c2831a46ef9ba1688587ea7403'
+	},
+	{
+		id: '301333',
+		name: 'Check-in (4)',
+		blockId: 'block-1b2d4a8a60354e42b33cd25348b0bad3'
+	},
+]
+
 let newRequest = (url) => {
 
 	var xhr = new XMLHttpRequest();
@@ -17,45 +30,45 @@ let newRequest = (url) => {
 	xhr.send();
 }
 
+const clickHandler = (mdmUrl, element) => {
+	newRequest(mdmUrl)
+	currHTML = element.innerHTML
+	element.innerHTML = 'Success!'
+	setTimeout(() => {
+		element.innerHTML = currHTML
+	},3000)
+}
+
+const createButton = (deviceID, deviceName) => {
+	const unlock = document.createElement('BUTTON')
+	unlock.id = `unlock-${deviceID}'`
+	unlock.classList.add(`unlock`)
+	unlock.innerHTML = `Unlock`
+	unlock.onclick = () => {clickHandler(`https://a.simplemdm.com/api/v1/device_groups/49646/devices/${deviceID}`, unlock)};
+
+	const lock = document.createElement('BUTTON')
+	lock.id = `lock-${deviceID}'`
+	lock.classList.add(`lock`)
+	lock.innerHTML = `Lock`
+	lock.onclick = () => {clickHandler(`https://a.simplemdm.com/api/v1/device_groups/49644/devices/${deviceID}`, lock)};
+
+	const reboot = document.createElement('BUTTON')
+	reboot.id = `reboot-${deviceID}'`
+	reboot.classList.add(`reboot`)
+	reboot.innerHTML = `Reboot`
+	reboot.onclick = () => clickHandler(`https://a.simplemdm.com/api/v1/devices/${deviceID}/restart`, reboot);
+
+	return [reboot, lock, unlock]
+}
 
 document.addEventListener('DOMContentLoaded',
 	() => {
-		const elem = document.getElementById('block-1b2d4a8a60354e42b33cd25348b0bad3')
-		
-		const clickHandler = (mdmUrl, element) => {
-			newRequest(mdmUrl)
-			currHTML = element.innerHTML
-			element.innerHTML = 'Success!'
-			setTimeout(() => {
-				element.innerHTML = currHTML
-			},3000)
-		}
+		stations.forEach((station) => {
+			let elem = document.getElementById(station.blockId)
 
-		const createButton = (deviceID, deviceName) => {
-			const unlock = document.createElement('BUTTON')
-			unlock.id = `unlock-${deviceID}'`
-			unlock.classList.add(`unlock`)
-			unlock.innerHTML = `Unlock`
-			unlock.onclick = () => {clickHandler(`https://a.simplemdm.com/api/v1/device_groups/49646/devices/${deviceID}`, unlock)};
-
-			const lock = document.createElement('BUTTON')
-			lock.id = `lock-${deviceID}'`
-			lock.classList.add(`lock`)
-			lock.innerHTML = `Lock`
-			lock.onclick = () => {clickHandler(`https://a.simplemdm.com/api/v1/device_groups/49644/devices/${deviceID}`, lock)};
-
-			const reboot = document.createElement('BUTTON')
-			reboot.id = `reboot-${deviceID}'`
-			reboot.classList.add(`reboot`)
-			reboot.innerHTML = `Reboot`
-			reboot.onclick = () => clickHandler(`https://a.simplemdm.com/api/v1/devices/${deviceID}/restart`, reboot);
-
-			return [reboot, lock, unlock]
-		}
-
-
-		createButton('301333', 'Check-in (4)').forEach((button) => {
-			elem.insertAdjacentElement('afterend', button)
+			createButton(station.id, station.name).forEach((button) => {
+				elem.insertAdjacentElement('afterend', button)
+			})
 		})
 	}
 )
@@ -64,4 +77,4 @@ document.addEventListener('DOMContentLoaded',
 // Self-hosted proxy: https://cors-anywhere-shiloh.herokuapp.com/
 // js file CDN through: https://combinatronics.com/
 // js CDN URL: https://www.combinatronics.com/lukecparr/shiloh-simplemdm-control/main/create-buttons.js
-{/* <script src='https://www.combinatronics.com/lukecparr/shiloh-simplemdm-control/main/create-buttons.js'></script> */}
+// <script src='https://www.combinatronics.com/lukecparr/shiloh-simplemdm-control/main/create-buttons.js'></script>
